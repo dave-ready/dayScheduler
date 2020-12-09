0389b4340d7c30693e60aec7ede449d8a4783cf2
dave ready <davis_ready@hotmail.com>
2020-12-09 18:54:20 -0500

    displayCurrentDate() function complete

--- script.js
+++ script.js
@@ -4,9 +4,15 @@
 
 //create function to display the current date (moment.js??)
 
-function displayCurrentDate()
+function displayCurrentDate() {
+var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+$("#currentDay").text(currentDate);
+console.log(currentDate);
 
-var currentDate = 
+}
+
+displayCurrentDate()
+
 
 
 //WHEN I scroll down
@@ -15,13 +21,15 @@ var currentDate =
 //Create time blocks - style.css looks like it already has the styled elements -
 //do I create div/column/row/etc. element classes/id's for them to style??
 
-var timeBlock =  ;
+//var amHours = ["1:00am", "2:00am", "3:00am", "4:00am", "5:00am", "6:00am", "7:00am", "8:00am", "9:00am", "10:00am", "11:00am", "12:00am"];
+//var pmHours = ["1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm", "6:00pm", "7:00pm", "8:00pm", "9:00pm", "10:00pm", "11:00pm", "12:00pm"];
+//var timeBlock =  ;
 
 //WHEN I view the time blocks for that day
 //THEN each time block is color-coded to indicate whether it is in the past, present, or future
-var past = ;
-var present = ;
-var future =  ;
+//var past = ;
+//var present = ;
+//var future =  ;
 
 //if (timeBlock < currentDate) {"past"}
 //else if (timeBlock === currentDate) {"present"}
@@ -30,7 +38,7 @@ var future =  ;
 //WHEN I click into a time block
 //THEN I can enter an event
 
-function createEvent()
+//function createEvent()
  
 //WHEN I click the save button for that time block
 
@@ -39,7 +47,7 @@ function createEvent()
 
 //create saveBTN
 
-function saveEvent()
+//function saveEvent()
 
 //WHEN I refresh the page
 //THEN the saved events persist'
