b7d8b8694d1599a40f163b217d8cf48764017e72
dave ready <davis_ready@hotmail.com>
2020-12-12 15:11:39 -0500

    cleanup js

--- script.js
+++ script.js
@@ -1,51 +1,30 @@
 
-//GIVEN I am using a daily planner to create a schedule
-//WHEN I open the planner
-//THEN the current day is displayed at the top of the calendar
-//WHEN I scroll down
-//THEN I am presented with time blocks for standard business hours -- 9am-5pm
-//WHEN I view the time blocks for that day
-//THEN each time block is color-coded to indicate whether it is in the past, present, or future
-//WHEN I click into a time block
-//THEN I can enter an event
-//WHEN I click the save button for that time block
-//THEN the text for that event is saved in local storage
-//WHEN I refresh the page
-//THEN the saved events persist
-
-
+// .ready() function to make sure DOM is loaded brfore running javascript
 $(document).ready(function() { 
 
 //create function to display the current date using moment.js
-
 function displayCurrentDate() {
 var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 $("#currentDay").text(currentDate);
 console.log(currentDate);
-
 }
 
 //runs the function
 displayCurrentDate()
 
-
-
-
-
 //create variables + array to loop through day calendar and store
-
-var currentHour = moment().format("ddd, HH");
+var currentHour = moment().format("hA");
 
 //added event with empty values (" ") for event data/text
-var workHours = [ {calRow: "0", hour: "09", time:"9",   amPM: "AM", eventDetails:" "},
-                  {calRow: "1", hour: "10", time: "10", amPM: "AM", eventDetails:" "}, 
-                  {calRow: "2", hour: "11", time: "11", amPM: "AM", eventDetails:" "}, 
-                  {calRow: "3", hour: "12", time: "12", amPM: "PM", eventDetails:" "}, 
-                  {calRow: "4", hour: "13", time: "1",  amPM: "PM", eventDetails:" "}, 
-                  {calRow: "5", hour: "14", time: "2",  amPM: "PM", eventDetails:" "},
-                  {calRow: "6", hour: "15", time: "3",  amPM: "PM", eventDetails:" "}, 
-                  {calRow: "7", hour: "16", time: "4",  amPM: "PM", eventDetails:" "}, 
-                  {calRow: "8", hour: "17", time: "5",  amPM: "PM", eventDetails:" "}]
+var workHours =[{hour: "09", time: "9AM",  eventDetails:" "},
+                {hour: "10", time: "10AM", eventDetails:" "}, 
+                {hour: "11", time: "11AM", eventDetails:" "}, 
+                {hour: "12", time: "12PM", eventDetails:" "}, 
+                {hour: "13", time: "1PM",  eventDetails:" "}, 
+                {hour: "14", time: "2PM",  eventDetails:" "},
+                {hour: "15", time: "3PM",  eventDetails:" "}, 
+                {hour: "16", time: "4PM",  eventDetails:" "}, 
+                {hour: "17", time: "5PM",  eventDetails:" "}]
 
  console.log(currentHour);  
                
@@ -59,28 +38,30 @@ $(".container").append(timeBlock);
 
 // create text field for time block
 var timeField = $("<div>")
-.text(hour.time + hour.amPM)
+.text(hour.time)
 .attr({"class": "col-md-2"});
+console.log(hour.time);
+
 
 // create event block
 var eventBlock = $("<div>").attr({"class": "col-md-9 description p-0"});
 var eventData = $("<textarea>");
 eventBlock.append(eventData);
-eventData.attr("id", hour.id);
+eventData.attr("id", hour.time);
 
 console.log(eventData); 
 
-//if/else statement to mark past,present,future
-if (hour.time <= moment().format("h")) {
+//if/else statement to mark past,present,future --- why is 9am always coming up as "future"????
+if (hour.time < currentHour) {
     eventData.attr({"class": "past"})
 
 } 
-else if (hour.time === moment().format("h")) {
+else if (hour.time === currentHour) {
     eventData.attr({"class": "present"})
 
 } 
 
-else if (hour.time > moment().format("h")) {
+else if (hour.time > currentHour) {
     eventData.attr({"class": "future"})
 
 }
@@ -100,11 +81,11 @@ function saveEvent() {
     localStorage.setItem("workHours", JSON.stringify(workHours));
 } 
 
-//create postEvent() function to keep the data on the page
+//create postEvent() function to keep the data on the page   
 //after it's stored in local storage, this function should set it to eventBlock.
 function postEvent() {
-    workHours.forEach(function (_thisHour) {
-        $("#_thisHour.id").val(_thisHour.eventDetails);
+    workHours.forEach(function (hour) {
+        $("#hour.id").val(hour.eventDetails);
     })
 } 
 //if there is any data in local storage
@@ -119,12 +100,11 @@ function init() {
  saveEvent()
  postEvent()
 }
-
+init();
 //.on("click") for functioning save button
 
 $(".saveBtn").on("click", function(event) {
     event.preventDefault();
-
     var saveData = $(this).siblings(".description").children(".future").attr("id");
     workHours[saveData].eventDetails = $(this).siblings(".description").children(".future").val();
     console.log(saveData);
